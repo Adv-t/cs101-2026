746925375aa9c61093777401648ea781b3b399cb
Adv-t <v0905787063@gmail.com>
2026-09-24 14:03:52 +0800

    Create main.c
    main.c

---
+++ EX1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+   printf("\t\t2026 9月\n");
+   printf("日\t一\t二\t三\t四\t五\t六\n");
+   printf("\033[1;34m30");
+   printf("\033[1;36m\t31\t 1\t 2\t 3\t 4\t 5\n");
+   printf(" 6\t 7\t 8\t 9\t10\t11\t12\n");
+   printf("13\t14\t15\t16\t17\t18\t19\n");
+   printf("20\t21\t22\t23\t24\t25\t26\n");
+   printf("27\t28\t29\t");
+   printf("\033[1;34m30\t 1\t 2\t 3\n");
+   
+    return 0;
+}
